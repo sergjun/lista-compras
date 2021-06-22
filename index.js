8d0a4f4ceefe8932b354649cff93c19cfa3ec9c8
sergjun <serrgio.madureira@gmail.com>
2021-06-22 01:48:02 -0300

    criado um formulario que registra infos no local store e exibe com a criacao de uma nova div

---
+++ index.js
@@ -0,0 +1,66 @@
+
+const mainForm = document.getElementById("main-form")
+const gravarBotao = document.getElementById("gravarBotao")
+const exibirBotao = document.getElementById("mostrarLista")
+
+
+
+
+gravarBotao.addEventListener ("click", () => gravarItem()) 
+
+mostrarLista.addEventListener ("click", () => exibirLista ())
+
+
+function gravarItem ()
+{
+
+    const numeroItem = document.getElementById("numero-item")
+    const nome = document.getElementById("nome")
+    const item = document.getElementById ("item")
+
+
+
+
+    const listaObjeto = { 
+    "Nome" : nome.value ,   
+    "Item" : item.value ,
+    "Quantidade": numeroItem.value
+ 
+    }
+    const listaString = JSON.stringify(listaObjeto)
+
+
+    item.value = ""
+    numeroItem.value =""
+
+    localStorage.setItem("lista", listaString)
+
+    // console.log(`${nome.value} e ${item.value} ${numeroItem.value} ${listaString}` )
+    
+}
+
+
+function exibirLista ()
+{
+    const listaContainer = document.createElement ('div')
+    const nomeLista = document.createElement('p')
+    const itemsLista = document.createElement ('p')
+    const quantidadeItem = document.createElement ('p')
+
+    listaContainer.setAttribute ('class' ,'listaContainer')
+
+    const stringStorage = localStorage.getItem ("lista")
+
+    const mostrarStorage = JSON.parse(stringStorage)
+
+
+    nomeLista.textContent = mostrarStorage.Nome
+    itemsLista.textContent = mostrarStorage.Item, 
+    quantidadeItem.textContent = mostrarStorage.Quantidade
+    
+    mainForm.appendChild(listaContainer)
+ 
+    listaContainer.appendChild(nomeLista)
+    listaContainer.appendChild(itemsLista)
+    listaContainer.appendChild(quantidadeItem)
+}
